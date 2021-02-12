8966a79c950b6c28ba30bb99f55a942c67d6ac95
Hyper Water Dog <omegaflakes@gmail.com>
2021-02-12 15:02:45 -0800

    added visit and story pages

--- src/modules/story.js
+++ src/modules/story.js
@@ -1,8 +1,15 @@
 function createStory(){
     let story = document.createElement('div');
     story.setAttribute('class','story');
-    story.textContent="story";
 
+    let header = document.createElement('h1');
+    header.textContent="Our Story";
+    story.appendChild(header);
+
+    let storyText = document.createElement('p');
+    storyText.textContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    storyText.setAttribute('id','storyText');
+    story.appendChild(storyText);
     return story
 }
 
--- src/modules/visit.js
+++ src/modules/visit.js
@@ -1,8 +1,15 @@
 function createVisit(){
     let visit = document.createElement('div');
     visit.setAttribute('class','visit');
-    visit.textContent="visit";
 
+    let header = document.createElement('h1');
+    header.textContent="Visit Us";
+    visit.appendChild(header);
+
+    let visitText = document.createElement('p');
+    visitText.textContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    visitText.setAttribute('id','visitText');
+    visit.appendChild(visitText);
     return visit
 }
 
